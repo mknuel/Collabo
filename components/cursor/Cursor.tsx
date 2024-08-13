dccf9bd94cb93573540aee30c8590ba2d3204a03
mknuel <your_email_address@example.com>
2024-08-13 14:00:48 +0100

    added live functionality, chat, see others cursors

--- components/cursor/Cursor.tsx
+++ components/cursor/Cursor.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import CursorSVG from "@/public/assets/CursorSVG";
 
-function cursor() {
-	return <div>cursor</div>;
+type Props = {
+	x: number;
+	y: number;
+	color: string;
+	message: string;
+};
+
+function Cursor({ color, x, y, message }: Props) {
+	return (
+		<div
+			className="pointer-events-none absolute top-0 left-0"
+			style={{
+				transform: `translate(${x}px, ${y}px)`,
+			}}>
+            <CursorSVG color={color} />
+            
+            {/* Message */}
+		</div>
+	);
 }
 
-export default cursor;
+export default Cursor;
