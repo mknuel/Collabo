96fc8523315b8535041135d874c23af903e6a3a1
mknuel <your_email_address@example.com>
2024-08-20 14:13:53 +0100

    live fns

--- components/cursor/CursorChat.tsx
+++ components/cursor/CursorChat.tsx
@@ -25,7 +25,7 @@ function CursorChat({
 		if (e.key === "Enter") {
 			setCursorState({
 				mode: CursorMode.Chat,
-				previousMessage: cursorState?.message,
+				previousMessage: cursorState.message,
 				message: "",
 			});
 		} else if (e.key === "Escape") {
